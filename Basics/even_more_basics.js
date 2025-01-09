e79ccc4c147f416b62a81e19c3acced0c288dc60
GMDLULI <gugulethumdluli@gmail.com>
2025-01-09 19:57:22 +0200

    created a stopwatch program using html, css and javascript

--- Basics/even_more_basics.js
+++ Basics/even_more_basics.js
@@ -1,283 +1,283 @@
-// // static - keyword used to describe properties or methods that belong to a class 
-// //         rather then the objects created from that class.
+// static - keyword used to describe properties or methods that belong to a class 
+//         rather then the objects created from that class.
 
-// class MathUtil{
-//     static PI = 3.14159;
-//     static getDiameter(radius){
-//         return radius * 2
-//     }
-//     static getcircumfarance(radius){
-//         return 2 * this.PI * radius
-//     }
-// }
+class MathUtil{
+    static PI = 3.14159;
+    static getDiameter(radius){
+        return radius * 2
+    }
+    static getcircumfarance(radius){
+        return 2 * this.PI * radius
+    }
+}
 
-// // console.log(MathUtil.PI);// called by the class name 
-// // console.log(MathUtil.getDiameter(18));
-// // console.log(MathUtil.getcircumfarance(12))
+// console.log(MathUtil.PI);// called by the class name 
+// console.log(MathUtil.getDiameter(18));
+// console.log(MathUtil.getcircumfarance(12))
 
-// //inheritance - allows a class to inherit properties and methods of another class
+//inheritance - allows a class to inherit properties and methods of another class
 
-// class Animal{
-//     alive = true;
+class Animal{
+    alive = true;
 
-//     eat(){
-//         console.log(`This ${this.name} is eating`);
-//     }
-//     sleep(){
-//         console.log(`This ${this.name} is sleeping`);
-//     }
-// }
+    eat(){
+        console.log(`This ${this.name} is eating`);
+    }
+    sleep(){
+        console.log(`This ${this.name} is sleeping`);
+    }
+}
 
-// class Dog extends Animal{
-//     name  = 'Dog';
-// }
+class Dog extends Animal{
+    name  = 'Dog';
+}
 
-// class Cat extends Animal{
-//     name  = 'Cat';
-// }
+class Cat extends Animal{
+    name  = 'Cat';
+}
 
-// class Snake extends Animal{
-//     name  = 'Snake';
-// }
+class Snake extends Animal{
+    name  = 'Snake';
+}
 
-// const dog = new Dog();
-// const cat = new Cat();
-// const snake = new Snake();
+const dog = new Dog();
+const cat = new Cat();
+const snake = new Snake();
 
-// // console.log(dog.eat());
-// // console.log(dog.sleep());
-// // console.log(cat.eat());
-// // console.log(cat.sleep());
-// // console.log(snake.eat());
-// // console.log(snake.sleep());
+// console.log(dog.eat());
+// console.log(dog.sleep());
+// console.log(cat.eat());
+// console.log(cat.sleep());
+// console.log(snake.eat());
+// console.log(snake.sleep());
 
-// // super- key word to call the constructor of the parent class and access the 
-// //        properties and methods of the parent (superclass)
+// super- key word to call the constructor of the parent class and access the 
+//        properties and methods of the parent (superclass)
 
-// class Animal2{
-//     constructor(name , age){
-//         this.name = name; 
-//         this.age = age;
-//     }
+class Animal2{
+    constructor(name , age){
+        this.name = name; 
+        this.age = age;
+    }
 
-//     move(speed){
-//         console.log(`This ${this.name} moves ${speed} mph`)
-//     }
-// }
+    move(speed){
+        console.log(`This ${this.name} moves ${speed} mph`)
+    }
+}
 
-// class Rabbit extends Animal2{
-//     constructor(name, age ,runspeed){
-//         super(name, age);
-//         this.runspeed = runspeed;
-//     }
+class Rabbit extends Animal2{
+    constructor(name, age ,runspeed){
+        super(name, age);
+        this.runspeed = runspeed;
+    }
 
-//     run(){
-//         console.log(`this ${this.name} can run.`);
-//         super.move(this.runspeed) // to call a method from the parent class 
-//     }
+    run(){
+        console.log(`this ${this.name} can run.`);
+        super.move(this.runspeed) // to call a method from the parent class 
+    }
 
-// }
-// class Fish extends Animal2{
-//     constructor(name, age, swimspeed){
-//         super(name, age);
-//         this.swimspeed = swimspeed;
-//     }
-//     swim(){
-//         console.log(`this ${this.name} can swim.`);
-//         super.move(this.swimspeed) // to call a method from the parent class 
-//     }
+}
+class Fish extends Animal2{
+    constructor(name, age, swimspeed){
+        super(name, age);
+        this.swimspeed = swimspeed;
+    }
+    swim(){
+        console.log(`this ${this.name} can swim.`);
+        super.move(this.swimspeed) // to call a method from the parent class 
+    }
 
 
-// }
-// class Hawk extends Animal2{
-//     constructor(name, age, flyspeed){
-//         super(name, age);
-//         this.flyspeed = flyspeed;
-//     }
-//     fly(){
-//         console.log(`this ${this.name} can fly.`);
-//         super.move(this.flyspeed) // to call a method from the parent class 
-//     }
+}
+class Hawk extends Animal2{
+    constructor(name, age, flyspeed){
+        super(name, age);
+        this.flyspeed = flyspeed;
+    }
+    fly(){
+        console.log(`this ${this.name} can fly.`);
+        super.move(this.flyspeed) // to call a method from the parent class 
+    }
 
-// }
+}
 
-// const rabbit = new Rabbit("Bunny", 1, 25);
-// const fish = new Fish("nemo", 1, 15);
-// const hawk = new Hawk("Hawk", 10, 50);
+const rabbit = new Rabbit("Bunny", 1, 25);
+const fish = new Fish("nemo", 1, 15);
+const hawk = new Hawk("Hawk", 10, 50);
 
-// //console.log(rabbit.name);
-// // console.log(rabbit.age);
-// // console.log(rabbit.runspeed);
-// // rabbit.run();
-// // console.log(fish.name);
-// // console.log(fish.age);
-// // console.log(fish.swimspeed);
-// // fish.swim();
-// // console.log(hawk.name);
-// // console.log(hawk.age);
-// // console.log(hawk.flyspeed);
-// // hawk.fly();
+//console.log(rabbit.name);
+// console.log(rabbit.age);
+// console.log(rabbit.runspeed);
+// rabbit.run();
+// console.log(fish.name);
+// console.log(fish.age);
+// console.log(fish.swimspeed);
+// fish.swim();
+// console.log(hawk.name);
+// console.log(hawk.age);
+// console.log(hawk.flyspeed);
+// hawk.fly();
 
-// //getters = method that makes a property readable
-// // setters = method that makes a property writable
+//getters = method that makes a property readable
+// setters = method that makes a property writable
 
-// class Rectangle{
+class Rectangle{
 
-//     constructor(width, height){
-//         this.width = width;
-//         this.height = height;
-//     }
-//     // setter
-//     set width(newwidth){
-//         if (newwidth > 0){ 
-//             this._width = newwidth
-//         }else{
-//             return 'undefined number'
-//         }
-//     }
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+    // setter
+    set width(newwidth){
+        if (newwidth > 0){ 
+            this._width = newwidth
+        }else{
+            return 'undefined number'
+        }
+    }
 
-//      // setter
-//      set height(newheight){
-//         if (newheight > 0){
-//             this._height = newheight;
-//         }else{
-//             return 'undefined number'
-//         }
-//     }
+     // setter
+     set height(newheight){
+        if (newheight > 0){
+            this._height = newheight;
+        }else{
+            return 'undefined number'
+        }
+    }
 
-//     // getter
-//     get width(){
-//         return this._width;
+    // getter
+    get width(){
+        return this._width;
        
-//     }
+    }
 
-//     // getter
-//     get height(){
-//          return this._height;
+    // getter
+    get height(){
+         return this._height;
        
-//     }
-// }
+    }
+}
 
-// const rectangle = new Rectangle(3, 4)
-// rectangle.width = -9;
-// rectangle.height = "Pizza";
-// console.log(rectangle.width);
-// console.log(rectangle.height);
+const rectangle = new Rectangle(3, 4)
+rectangle.width = -9;
+rectangle.height = "Pizza";
+console.log(rectangle.width);
+console.log(rectangle.height);
 
-// // destructuring = extract values from arrays and objects,
-// //                then assign them to variables in a convient way
-// //                [] = to perform array destructuring
-// //                {} = to perform object destructuring 
+// destructuring = extract values from arrays and objects,
+//                then assign them to variables in a convient way
+//                [] = to perform array destructuring
+//                {} = to perform object destructuring 
 
 
-// // swap the value of two variables
+// swap the value of two variables
 
-// let a = 1;
-// let b = 2;
+let a = 1;
+let b = 2;
 
-// [a, b] = [b, a]
-// console.log(a); // return 2
-// console.log(b); // return 1
+[a, b] = [b, a]
+console.log(a); // return 2
+console.log(b); // return 1
 
-// // using a list of colors 
-// const colors = ['red', 'green', 'blue', 'black', 'white'];
+// using a list of colors 
+const colors = ['red', 'green', 'blue', 'black', 'white'];
 
-// [colors[0], colors[4]] = [colors[4], colors[0]]// swapping the first and last element
-// console.log(colors);
+[colors[0], colors[4]] = [colors[4], colors[0]]// swapping the first and last element
+console.log(colors);
 
-// const [firstcolor, secondcolor, thirdcolor, ...extracolors] = colors;
-// console.log(firstcolor); // withh return the first element of the colors array
-// console.log(secondcolor); // will return the 2nd element of the colors array
-// console.log(thirdcolor); // will return 3rd element of the colors array
-// console.log(extracolors); // will return the remaining elements 
+const [firstcolor, secondcolor, thirdcolor, ...extracolors] = colors;
+console.log(firstcolor); // withh return the first element of the colors array
+console.log(secondcolor); // will return the 2nd element of the colors array
+console.log(thirdcolor); // will return 3rd element of the colors array
+console.log(extracolors); // will return the remaining elements 
 
-// // with objects 
-// const person1 = {
-//     firstname: 'Gugu',
-//     lastname: 'Mdluli',
-//     age: 26,
-//     job: "software Dev"
-// }
+// with objects 
+const person1 = {
+    firstname: 'Gugu',
+    lastname: 'Mdluli',
+    age: 26,
+    job: "software Dev"
+}
 
-// const person2 = {
-//     firstname: 'Motheo',
-//     lastname: 'Mokoena',
-//     age: 24,
-//     job: "Teacher"
-// }
+const person2 = {
+    firstname: 'Motheo',
+    lastname: 'Mokoena',
+    age: 24,
+    job: "Teacher"
+}
 
-// const {firstname, lastname, age, job} = person1;
-// console.log(firstname);
-// console.log(lastname);
-// console.log(age);
-// console.log(job);
+const {firstname, lastname, age, job} = person1;
+console.log(firstname);
+console.log(lastname);
+console.log(age);
+console.log(job);
 
-// function displayPerson({firstname, lastname, age, job}){
-//     console.log(`name: ${firstname} ${lastname}`);
-//     console.log(`age: ${age}`);
-//     console.log(`job: ${job}`);
-// }
+function displayPerson({firstname, lastname, age, job}){
+    console.log(`name: ${firstname} ${lastname}`);
+    console.log(`age: ${age}`);
+    console.log(`job: ${job}`);
+}
 
-// displayPerson(person2)
+displayPerson(person2)
 
-// // nested objects = objects inside of other objects.
-// //                  Allows you to represent more complex data structured
-// //                  child object is indide or enclosed by the parent object
-// //                  Person{address{}, contactInfo{}}
+// nested objects = objects inside of other objects.
+//                  Allows you to represent more complex data structured
+//                  child object is indide or enclosed by the parent object
+//                  Person{address{}, contactInfo{}}
 
-// const persopn3 = {
-//     fullname: 'Gugulethu mdluli',
-//     age: 26,
-//     hobbies: ['cooking', 'reading', 'playing chess'],
-//     address: {
-//         streetName: 'tswako',
-//         streetNumber: 12345,
-//         country: 'South Africa'
-//     },
-//     contact: '076510034'
-// }
+const persopn3 = {
+    fullname: 'Gugulethu mdluli',
+    age: 26,
+    hobbies: ['cooking', 'reading', 'playing chess'],
+    address: {
+        streetName: 'tswako',
+        streetNumber: 12345,
+        country: 'South Africa'
+    },
+    contact: '076510034'
+}
 
-// // to access properies individually
-// console.log(persopn3.fullname);
-// console.log(persopn3.age);
-// // using indexing to access list/array properties 
-// console.log(persopn3.hobbies[1]); // will return reading
-// // accessing child object properties 
-// console.log(persopn3.address.streetName);
-// //using a loop to access all the properties 
-// for(const prop in persopn3.address){
-//     console.log(persopn3.address[prop])
-// }
+// to access properies individually
+console.log(persopn3.fullname);
+console.log(persopn3.age);
+// using indexing to access list/array properties 
+console.log(persopn3.hobbies[1]); // will return reading
+// accessing child object properties 
+console.log(persopn3.address.streetName);
+//using a loop to access all the properties 
+for(const prop in persopn3.address){
+    console.log(persopn3.address[prop])
+}
 
-// //nested objects with classes 
+//nested objects with classes 
 
-// class Person3 {
-//     constructor(fullname, age, ...address){
-//         this.fullname = fullname,
-//         this.age = age,
-//         this.address = new Address(...address)
-//     }
-// }
+class Person3 {
+    constructor(fullname, age, ...address){
+        this.fullname = fullname,
+        this.age = age,
+        this.address = new Address(...address)
+    }
+}
 
-// class Address{
-//     constructor(street, city,country){
-//         this.street = street,
-//         this.city = city,
-//         this.country = country
-//     }
-// }
+class Address{
+    constructor(street, city,country){
+        this.street = street,
+        this.city = city,
+        this.country = country
+    }
+}
 
-// const gugu = new Person3("Gugulethu Mdluli",26, "tswako",
-//                                                 'Johannesburg',
-//                                                 "South Africa"
-//  )
+const gugu = new Person3("Gugulethu Mdluli",26, "tswako",
+                                                'Johannesburg',
+                                                "South Africa"
+ )
 
-//  console.log(gugu.fullname);
-//  console.log(gugu.age);
-//  console.log(gugu.address.country);
-//  for (const me in gugu.address){
-//     console.log(gugu.address[me]);
-//  }
+ console.log(gugu.fullname);
+ console.log(gugu.age);
+ console.log(gugu.address.country);
+ for (const me in gugu.address){
+    console.log(gugu.address[me]);
+ }
 
 
  const fruits = [
---
+++ stop_watch/stop_watch.js
@@ -0,0 +1,50 @@
+const display = document.getElementById('stopwatch');
+let timer = 0;
+let startTime = 0;
+let elapsedTime = 0;
+let isRuning = false;
+
+function start(){
+    
+    if(!isRuning){
+        startTime = Date.now() - elapsedTime;
+        timer = setInterval(updateDisplay, 10);
+        isRuning = true;
+    }
+}
+
+function stop(){
+    if(isRuning){
+        clearInterval(timer);
+        elapsedTime = Date.now() - startTime;
+        isRuning = false;
+    }
+
+}
+
+function reset(){
+    clearInterval(timer)
+    startTime = 0;
+    elapsedTime = 0;
+    isRuning = false;
+    display.textContent = '00:00:00'
+
+}
+
+function updateDisplay(){
+    const currentTime = Date.now();
+    elapsedTime = currentTime - startTime;
+
+    let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+    let minutes = Math.floor(elapsedTime / (1000 * 60) % 60);
+    let seconds = Math.floor(elapsedTime / 1000  % 60);
+    let milliseconds = Math.floor(elapsedTime % 1000 / 10);
+
+    hours = String(hours).padStart(2, "0");
+    minutes = String(minutes).padStart(2, "0");
+    seconds = String(seconds).padStart(2, "0");
+    milliseconds = String(milliseconds).padStart(2, 0);
+    display.textContent = `${hours}: ${minutes}: ${seconds}: ${milliseconds}`
+    console.log(`${hours}: ${minutes}: ${seconds}: ${milliseconds}`);
+
+}
